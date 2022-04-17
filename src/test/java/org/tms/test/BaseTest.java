36ada4cdae07b59df41e58934e9dc0f3e8cca601
nastassia <helgaGpotaki@mail.ru>
2022-04-17 11:33:04 +0300

    CartPageTest added (3 tests) InventoryPageTest added (2 tests)

--- src/test/java/org/tms/test/BaseTest.java
+++ src/test/java/org/tms/test/BaseTest.java
@@ -3,21 +3,23 @@ package org.tms.test;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.tms.driver.DriverSingleton;
 
 public class BaseTest {
 
     protected WebDriver driver;
 
-    @BeforeClass
+    @BeforeTest
     public void setUp() {
         driver = DriverSingleton.getDriver();
     }
 
-    @AfterClass
+    @AfterTest
     public void stopBrowser() {
         DriverSingleton.closeDriver();
+
     }
 
 }
